2bd0d878deba9e24fd86e7858cfe2502257e554a
Rob Meijer <rob@noemail>
2023-04-12 14:03:33 +0200

    building the 1.0.5 (working) and the 2.0.0 (currently broken)

--- git_info_fluree_latest.py
+++ git_info_fluree_latest.py
@@ -7,10 +7,14 @@ latest_date = ""
 latest_beta_date = ""
 latest_release = None
 latest_release = None
+no2 = False
+if len(sys.argv) > 1 and sys.argv[1] == "v1":
+    no2 = True
 for release in r.json():
     if release["published_at"] > latest_date and not release["prerelease"]:
-        latest_date = release["published_at"]
-        latest_release = release
+        if not ("v2" in release["tag_name"] and  no2):
+            latest_date = release["published_at"]
+            latest_release = release
     if release["published_at"] > latest_beta_date and release["prerelease"] and "beta" in release["tag_name"]:
         latest_beta_date = release["published_at"]
         latest_beta_release = release
--- push_docker.sh
+++ push_docker.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-echo Pushing stable
-docker push pibara/fsst:stable
-docker push pibara/fsst:v`python3 git_info_fluree_latest.py | sed -e 's/\r//'`
-docker push pibara/fsst:v`python3 -m pip index versions fsst|grep fsst |sed -e 's/.*(//' -e 's/).*//'`-stable
-docker push pibara/fsst:v`python3 -m pip index versions fsst|grep fsst |sed -e 's/.*(//' -e 's/).*//'`-`python3 git_info_fluree_latest.py | sed -e 's/\r//'`
-echo Pushing beta
-docker push pibara/fsst:beta
-docker push pibara/fsst:v`python3 -m pip index versions fsst|grep fsst |sed -e 's/.*(//' -e 's/).*//'`-beta
-docker push pibara/fsst:`python3 git_info_fluree_latest.py beta| sed -e 's/\r//'`
-docker push pibara/fsst:v`python3 -m pip index versions fsst|grep fsst |sed -e 's/.*(//' -e 's/).*//'`-`python3 git_info_fluree_latest.py beta| sed -e 's/\r//'`
+echo Pushing stable-broken
+docker push pibara/fsst:stable-broken
+echo Pushing v1
+docker push pibara/fsst:v1
+docker push pibara/fsst:v`python3 -m pip index versions fsst|grep fsst |sed -e 's/.*(//' -e 's/).*//'`-v1
+docker push pibara/fsst:`python3 git_info_fluree_latest.py v1| sed -e 's/\r//'`
