30ebd2bafd04ebdc2768d2165d24f367d0b12ca5
RJ Meijer <pibara@gmail.com>
2023-09-13 09:40:15 +0200

    added an attempt at a docker file for building trunk, not yet working

--- git_clone_fluree_latest.py
+++ git_clone_fluree_latest.py
@@ -8,20 +8,26 @@ latest_beta_date = ""
 latest_release = None
 latest_beta_release = None
 no2 = False
+usetag = True
 if len(sys.argv) > 1 and sys.argv[1] == "v1":
     no2 = True
-for release in r.json():
-    if release["published_at"] > latest_date and not release["prerelease"]:
-        if not ("v2" in release["tag_name"] and  no2):
-            latest_date = release["published_at"]
-            latest_release = release
-    if release["published_at"] > latest_beta_date and release["prerelease"] and "beta" in release["tag_name"]:
-        latest_beta_date = release["published_at"]
-        latest_beta_release = release
-if len(sys.argv) > 1 and sys.argv[1] == "beta":
-    print(latest_beta_release["tag_name"])
-    latest_release = latest_beta_release
+if len(sys.argv) > 1 and sys.argv[1] == "snapshot":
+    usetag = False
+if usetag:
+    for release in r.json():
+        if release["published_at"] > latest_date and not release["prerelease"]:
+            if not ("v2" in release["tag_name"] and  no2):
+                latest_date = release["published_at"]
+                latest_release = release
+        if release["published_at"] > latest_beta_date and release["prerelease"] and "beta" in release["tag_name"]:
+            latest_beta_date = release["published_at"]
+            latest_beta_release = release
+    if len(sys.argv) > 1 and sys.argv[1] == "beta":
+        print(latest_beta_release["tag_name"])
+        latest_release = latest_beta_release
+    else:
+        print(latest_release["tag_name"])
+    command = ["/usr/bin/git", "clone", "--depth", "1", "--branch", latest_release["tag_name"], "https://github.com/fluree/ledger.git", "."]
 else:
-    print(latest_release["tag_name"])
-command = ["/usr/bin/git", "clone", "--depth", "1", "--branch", latest_release["tag_name"], "https://github.com/fluree/ledger.git", "."]
+    command = ["/usr/bin/git", "clone", "--depth", "1", "https://github.com/fluree/ledger.git", "."]
 subprocess.run(command)
