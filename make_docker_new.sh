6f2919578a2ed09ee3f798d40488e02dbd959422
RJ Meijer <pibara@gmail.com>
2024-03-25 18:07:15 +0100

    also use FlureePBC docker as parent image for 1.0.5 version

--- make_docker_new.sh
+++ make_docker_new.sh
@@ -5,3 +5,7 @@ docker build --no-cache -t pibara/fsst:stable . -f Dockerfile-latest-dockerhub
 docker tag pibara/fsst:stable pibara/fsst:`python3 dockerhub_info_fluree_latest.py | sed -e 's/\r//'` 
 docker tag pibara/fsst:stable pibara/fsst:v`python3 -m pip index versions fsst|grep fsst |sed -e 's/.*(//' -e 's/).*//'`-`python3 dockerhub_info_fluree_latest.py | sed -e 's/\r//'`
 docker tag pibara/fsst:stable pibara/fsst:v`python3 -m pip index versions fsst|grep fsst |sed -e 's/.*(//' -e 's/).*//'`-stable
+docker build --no-cache -t pibara/fsst:v1 . -f Dockerfile-v1-dockerhub
+docker tag pibara/fsst:v1 pibara/fsst:v`python3 -m pip index versions fsst|grep fsst |sed -e 's/.*(//' -e 's/).*//'`-v1
+docker tag pibara/fsst:v1 pibara/fsst:v1.0.5
+docker tag pibara/fsst:v1 pibara/fsst:v`python3 -m pip index versions fsst|grep fsst |sed -e 's/.*(//' -e 's/).*//'`-v1.0.5
